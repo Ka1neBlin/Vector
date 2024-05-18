01b8228f0bfa4773e4cd8daafd9bc41f866b1b60
Liza Belonina <fkme.fuhua@gmail.com>
2024-05-18 05:30:56 +0400

    Я пробовала функцию с return сделать

---
+++ Vector.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+template <typename T>
+void voidHuge(vector<T>& vec, const int size) {
+    // Здесь я на прямую меняю его размер
+    vec.resize(vec.size() + size);
+}
+int main()
+{
+    vector<int> first(5);
+    cout << "First size: " << first.size() << '\n';
+    voidHuge(first, 3);
+    cout << "First size: " << first.size() << '\n';
+
+    vector<string> second(5);
+    cout << "Second size: " << second.size() << '\n';
+    voidHuge(second, 4);
+    cout << "Second size: " << second.size() << '\n';
+}
