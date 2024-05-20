c575655ffe70a1b459988181089169e942581d04
Liza Belonina <fkme.fuhua@gmail.com>
2024-05-20 15:19:37 +0400

    Переделанные функции

--- Vector.cpp
+++ Vector.cpp
@@ -1,20 +1,40 @@
 ﻿#include <iostream>
 #include <vector>
 using namespace std;
-template <typename T>
-void voidHuge(vector<T>& vec, const int size) {
-    // Здесь я на прямую меняю его размер
-    vec.resize(vec.size() + size);
+void voidIncrease(vector<int>& vec, const int n) {
+    for (int i = 0; i < vec.size(); i++) {
+        vec[i] += n;
+    }
+}
+vector<int> increace(vector<int> vec, const int n) {
+    for (int i = 0; i < vec.size(); i++) {
+        vec[i] += n;
+    }
+    return vec;
 }
 int main()
 {
     vector<int> first(5);
-    cout << "First size: " << first.size() << '\n';
-    voidHuge(first, 3);
-    cout << "First size: " << first.size() << '\n';
+    vector<int> second(5);
+    cout << "First\n";
+    for (int i = 0; i < first.size(); i++) {
+        first[i] = rand() % 10;
+        cout << first[i] << ' ';
+    }
+    cout << "\nSecond\n";
+    for (int i = 0; i < second.size(); i++) {
+        second[i] = rand() % 10;
+        cout << second[i] << ' ';
+    }
 
-    vector<string> second(5);
-    cout << "Second size: " << second.size() << '\n';
-    voidHuge(second, 4);
-    cout << "Second size: " << second.size() << '\n';
+    cout << "\nUse return function\n";
+    vector<int> returned = increace(first, 1);
+    for (int i = 0; i < returned.size(); i++) {
+        cout << returned[i] << ' ';
+    }
+    cout << "\nUse void function\n";
+    voidIncrease(second, 2);
+    for (int i = 0; i < second.size(); i++) {
+        cout << second[i] << ' ';
+    }
 }
--- test.cpp
+++ test.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include <vector>
 TEST(TestCaseName, TestName) {
-	vector<int> test(4);
-	voidHuge(test, 6);
-	ASSERT_EQ(test.size(), 10);
+	vector<int> vec = { 1, 2, 3 };
+	vector<int> returned = increace(vec, 1);
+	ASSERT_EQ(returned[0], 2);
+	voidIncreace(vec, 2);
+	ASSERT_EQ(vec[0], 3);
 }
